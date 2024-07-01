b525887e362f17b3c285a148c58dea020d6f5c68
Rabah-Muhammed <rabarabi31@gmail.com>
2024-07-01 19:51:51 +0530

    removed secret

--- electro/electro/settings.py
+++ electro/electro/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*mw^dlvp8l2d==fat3vatou)8k!bc73mw2xtqe9o2n4f)1(xhq'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
